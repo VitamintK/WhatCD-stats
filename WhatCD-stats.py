8b2689507fc202fc14b46a754d33d76744030ef0
KEvin Wang <kvn.a.wng@gmail.com>
2014-06-21 01:41:15 -0700

    nightly commit
    Not functioning.

--- WhatCD-stats.py
+++ WhatCD-stats.py
@@ -5,6 +5,11 @@ import requests
 
 user = raw_input("username: ")
 passw = raw_input("password: ")
-payload = dict(username=user, password=passw)
-test = requests.post("http://what.cd/login.php",payload)
+payload = {'username':user, 'password':passw, 'keeplogged': 1, 'login': 'Login'}
+test = requests.post("https://ssl.what.cd/login.php",payload)
 print test.text
+print test.cookies
+
+print "--------- here starts it ----------"
+index = requests.get("http://what.cd/ajax.php?action=index",cookies=test.cookies)
+print index.text
