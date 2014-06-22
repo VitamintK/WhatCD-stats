6f3c20cdd6394e3bb247d6b75e07fe50312f3b50
KEvin Wang <kvn.a.wng@gmail.com>
2014-06-22 02:29:33 -0700

    copied code from whatapi
    copied code from
    https://github.com/isaaczafuta/whatapi/blob/master/whatapi/whatapi.py

--- WhatCD-stats.py
+++ WhatCD-stats.py
@@ -13,3 +13,36 @@ print test.cookies
 print "--------- here starts it ----------"
 index = requests.get("http://what.cd/ajax.php?action=index",cookies=test.cookies)
 print index.text
+
+class WhatAPI:
+    def __init__(self, config=None, username=None, password=None):
+        self.session = requests.Session()
+        self.session.headers = headers
+        self.authkey = None
+        self.passkey = None
+        if config:
+            config = ConfigParser()
+            config.read(config)
+            self.username = config.get('login', 'username')
+            self.password = config.get('login', 'password')
+        else:
+            self.username = username
+            self.password = password
+        self._login()
+
+    def _login(self):
+        '''Logs in user and gets authkey from server'''
+        loginpage = 'https://ssl.what.cd/login.php'
+        data = {'username': self.username,
+                'password': self.password,
+                'keeplogged': 1,
+                'login': 'Login'
+        }
+        r = self.session.post(loginpage, data=data, allow_redirects=False)
+        if r.status_code != 302:
+            raise LoginException
+        accountinfo = self.request("index")
+        self.authkey = accountinfo["response"]["authkey"]
+        self.passkey = accountinfo["response"]["passkey"]
+
+wcd = WhatAPI(username=user,password=passw)
